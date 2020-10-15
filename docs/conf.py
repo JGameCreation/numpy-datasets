b0eaa4cd2a1d4d60481d151486b1745f7c5631b5
RandallBalestriero <randallbalestriero@gmail.com>
2020-10-14 20:51:13 -0500

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ from sphinx_gallery.scrapers import matplotlib_scraper
 
 sys.path.insert(0, os.path.abspath(".."))
 
-MOCK_MODULES = ["soundfile", "tqdm", "imageio", "pretty-midi", "mne"]
+MOCK_MODULES = ["soundfile", "tqdm", "imageio", "pretty-midi", "mne", "h5py"]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
